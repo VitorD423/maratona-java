bd9e7ebfcaadeee60ea8e1d84abe1016da65bb00
VitorD423 <vitordiego200@gmail.com>
2025-11-23 11:28:52 -0300

    Estudo

--- src/academy/devdojo/maratonajava/javacore/Eblocosinicializacao/test/AnimeTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Eblocosinicializacao/test/AnimeTest01.java
@@ -4,7 +4,7 @@ import academy.devdojo.maratonajava.javacore.Eblocosinicializacao.domain.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime = new Anime();
+        Anime anime = new Anime("One Piece");
 
         for (int episodio : anime.getEpisodios()) {
             System.out.print(episodio + " ");
