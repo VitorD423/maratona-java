d661589d90208a798a83dcfdc3b24d349fe35c16
VitorD423 <vitordiego200@gmail.com>
2025-12-10 00:21:15 -0300

    Estudo

---
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/test/CarroTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Fmodificadorestatico.test;
+
+import academy.devdojo.maratonajava.javacore.Fmodificadorestatico.domain.Carro;
+
+public class CarroTest01 {
+    public static void main(String[] args) {
+        Carro c1 = new Carro("BWM", 280,);
+        Carro c1 = new Carro();
+        Carro c1 = new Carro();
+    }
+}
