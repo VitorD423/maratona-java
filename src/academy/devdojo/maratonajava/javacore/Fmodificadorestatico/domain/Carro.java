8d114920801a07b7963cea28050541d313b3213f
VitorD423 <vitordiego200@gmail.com>
2025-12-11 19:48:07 -0300

    Estudo

--- src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/domain/Carro.java
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/domain/Carro.java
@@ -3,21 +3,27 @@ package academy.devdojo.maratonajava.javacore.Fmodificadorestatico.domain;
 public class Carro {
     private String nome;
     private double velocidadeMaxima;
-    public static double velocidadeLimite = 250;
+    private static double velocidadeLimite = 250;
 
     public Carro(String nome, double velocidadeMaxima) {
         this.nome = nome;
         this.velocidadeMaxima = velocidadeMaxima;
     }
 
-    public void imprime(){
+    public static void imprime(){
         System.out.println("----------------");
         System.out.println("Nome "+this.nome);
         System.out.println("Velocidade máxima "+this.velocidadeMaxima);
         System.out.println("Velocidade limite "+Carro.velocidadeLimite);
     }
+    static public  void setVelocidadeLimite(double velocidadeLimite){
+        Carro.velocidadeLimite = velocidadeLimite;
+    }
 
 
+    public static double getVelocidadeLimite(){
+        return Carro.velocidadeLimite;
+    }
 
 
 
