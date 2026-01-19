1bd576678b6ae53ffd0d6dbbb4f352a81e1ee066
VitorD423 <vitordiego200@gmail.com>
2026-01-19 16:13:27 -0300

    Merge remote-tracking branch 'origin/master'

--- src/academy/devdojo/maratonajava/javacore/Gassociacao/dominio/Jogador.java
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/dominio/Jogador.java
@@ -1,11 +1,18 @@
 package academy.devdojo.maratonajava.javacore.Gassociacao.dominio;
 
 public class Jogador {
-    private  String nome;
+    private String nome;
+    private Time time;
 
     public void imprime(){
         System.out.println(this.nome);
+        if (time != null){
+            System.out.println(time.getNome());
+        }
     }
+
+
+
 
     public Jogador(String nome){
         this.nome = nome;
@@ -24,5 +31,14 @@ public class Jogador {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
     }
 }
