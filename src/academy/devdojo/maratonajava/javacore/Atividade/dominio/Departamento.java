349badc34502b2217e4aba505d7ebfb7c0292361
VitorD423 <vitordiego200@gmail.com>
2026-01-19 16:38:43 -0300

    Merge remote-tracking branch 'origin/master'

---
+++ src/academy/devdojo/maratonajava/javacore/Atividade/dominio/Departamento.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.Atividade.dominio;
+
+public class Departamento {
+    private String nome;
+
+    public Departamento(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+
+
+}
