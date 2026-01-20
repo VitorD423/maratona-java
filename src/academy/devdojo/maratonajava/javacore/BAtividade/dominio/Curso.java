2869e87f1f47df918a1220425d0442bebc865b58
VitorD423 <vitordiego200@gmail.com>
2026-01-20 14:42:24 -0300

    Merge remote-tracking branch 'origin/master'

--- src/academy/devdojo/maratonajava/javacore/BAtividade/dominio/Curso.java
+++ src/academy/devdojo/maratonajava/javacore/BAtividade/dominio/Curso.java
@@ -1,5 +1,34 @@
 package academy.devdojo.maratonajava.javacore.BAtividade.dominio;
 
 public class Curso {
+        private final String nome;
+        private Aluno[] alunos;
 
+    public Curso(String nome) {
+        this.nome = nome;
+    }
+
+
+
+
+    public void imprimirCurso(){
+        System.out.println("Curso: " + this.nome);
+        if (alunos == null)return;
+
+        System.out.println("Alunos: ");
+        for (Aluno aluno : alunos){
+            System.out.println(aluno.getNome());
+        }
+
+    }
+
+
+    public void setAlunos(Aluno[] alunos) {
+        this.alunos = alunos;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
 }
---
+++ src/academy/devdojo/maratonajava/javacore/BAtividade/test/CursoTest.java
@@ -0,0 +1,32 @@
+package academy.devdojo.maratonajava.javacore.BAtividade.test;
+
+import academy.devdojo.maratonajava.javacore.BAtividade.dominio.Aluno;
+import academy.devdojo.maratonajava.javacore.BAtividade.dominio.Curso;
+
+public class CursoTest {
+    public static void main(String[] args) {
+        Curso curso = new Curso("Java Backend");
+
+        Aluno aluno1 = new Aluno("Vitor");
+        Aluno aluno2 = new Aluno("Diego");
+        Aluno aluno3 = new Aluno("Torres");
+
+        Aluno[] alunos = {aluno1,aluno2,aluno3};
+
+        aluno1.setCurso(curso);
+        aluno2.setCurso(curso);
+        aluno3.setCurso(curso);
+
+        curso.setAlunos(alunos);
+
+        curso.imprimirCurso();
+        System.out.println();
+
+        aluno1.imprimirAluno();
+        System.out.println();
+        aluno2.imprimirAluno();
+        System.out.println();
+        aluno3.imprimirAluno();
+
+    }
+}
