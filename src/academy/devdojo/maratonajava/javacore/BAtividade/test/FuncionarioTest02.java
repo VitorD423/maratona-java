b437e27381d7978e8088167356a35abc52475943
VitorD423 <vitordiego200@gmail.com>
2026-01-22 12:24:52 -0300

    Merge remote-tracking branch 'origin/master'

---
+++ src/academy/devdojo/maratonajava/javacore/BAtividade/test/FuncionarioTest02.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.javacore.BAtividade.test;
+
+import academy.devdojo.maratonajava.javacore.BAtividade.dominio.Desenvolvedor;
+import academy.devdojo.maratonajava.javacore.BAtividade.dominio.Funcionario;
+import academy.devdojo.maratonajava.javacore.BAtividade.dominio.Gerente;
+
+public class FuncionarioTest02 {
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario("Vitor",2500);
+        Funcionario gerente = new Gerente("Jorge",30000,"ADM");
+        Funcionario desenvolvedorJunior = new Desenvolvedor("Diego",1500,"Junior");
+        Funcionario desenvolvedorSenior = new Desenvolvedor("Torres",3500,"Senior");
+
+        Funcionario[] funcionarios = {funcionario,gerente,desenvolvedorJunior,desenvolvedorSenior};
+
+        for (Funcionario f : funcionarios) {
+            System.out.println("------------");
+            f.imprimir();
+
+        }
+
+    }
+}
