d154cd5957dccfedf8e25cf6afb75b68a9a993bb
VitorD423 <vitordiego200@gmail.com>
2026-01-24 22:26:51 -0300

    Merge remote-tracking branch 'origin/master'

---
+++ src/academy/devdojo/maratonajava/javacore/BAtividade/dominio/Cliente.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.BAtividade.dominio;
+
+public class Cliente {
+    private String nome;
+
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "nome='" + nome + '\'' +
+                '}';
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/BAtividade/dominio/ContaPoupanca.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.BAtividade.dominio;
+
+public class ContaPoupanca extends ContaBancaria{
+
+    @Override
+    public void imprimirDados(){
+
+    }
+
+
+}
