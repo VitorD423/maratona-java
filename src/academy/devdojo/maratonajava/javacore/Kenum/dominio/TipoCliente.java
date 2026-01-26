e50e06fb6a615d9d3b05d4190df036c5dd812bfd
VitorD423 <vitordiego200@gmail.com>
2026-01-26 14:11:59 -0300

    Merge remote-tracking branch 'origin/master'

--- src/academy/devdojo/maratonajava/javacore/Kenum/dominio/TipoCliente.java
+++ src/academy/devdojo/maratonajava/javacore/Kenum/dominio/TipoCliente.java
@@ -8,6 +8,15 @@ public enum TipoCliente {
     private String nomeRelatorio;
     TipoCliente(int valor, String nomeRelatorio) {
        this.valor = valor;
+       this.nomeRelatorio = nomeRelatorio;
+    }
+    public static TipoCliente tipoClientePorNomeRelatorio(String nomeRelatorio){
+        for (TipoCliente tipoCliente : values()) {
+            if (tipoCliente.getNomeRelatorio().equals(nomeRelatorio)){
+                return tipoCliente;
+            }
+        }
+        return null;
     }
 
     public int getValor() {
