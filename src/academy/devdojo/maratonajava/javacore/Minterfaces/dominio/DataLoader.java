85e2ac93c57adff8481ab15a1738c2711c10a3c3
VitorD423 <vitordiego200@gmail.com>
2026-01-28 11:27:46 -0300

    Merge remote-tracking branch 'origin/master'

--- src/academy/devdojo/maratonajava/javacore/Minterfaces/dominio/DataLoader.java
+++ src/academy/devdojo/maratonajava/javacore/Minterfaces/dominio/DataLoader.java
@@ -1,8 +1,12 @@
 package academy.devdojo.maratonajava.javacore.Minterfaces.dominio;
 
 public interface DataLoader {
+    public static final int MAX_DATA_SIZE = 10;
     void load();
+
     default void checkPermission(){
         System.out.println("Fazendo checagem de permissões");
     }
+
+
 }
