6e1187f472fe19403f6fbb77d406cce4af9f9751
VitorD423 <vitordiego200@gmail.com>
2026-02-02 18:30:09 -0300

    Merge remote-tracking branch 'origin/master'

---
+++ src/academy/devdojo/maratonajava/Atividades/CAtividades/Execoes/test/test03.java
@@ -0,0 +1,8 @@
+package academy.devdojo.maratonajava.Atividades.CAtividades.Execoes.test;
+
+public class test03 {
+    public static void main(String[] args) {
+        dividir(10,0);
+        System.out.println("Fim");
+    }
+}
