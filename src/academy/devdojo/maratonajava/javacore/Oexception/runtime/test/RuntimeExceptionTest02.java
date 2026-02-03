03fb9876f10a3c2ca6427aad112e3e2458f26397
VitorD423 <vitordiego200@gmail.com>
2026-02-03 19:36:11 -0300

    Merge remote-tracking branch 'origin/master'

--- src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest02.java
@@ -2,15 +2,11 @@ package academy.devdojo.maratonajava.javacore.Oexception.runtime.test;
 
 public class RuntimeExceptionTest02 {
     public static void main(String[] args) {
-       try {
         divisao(1,0);
 
-       }catch (RuntimeException e){
-            e.printStackTrace();
-       }
         System.out.println("Código finalizado");
     }
-    private static int divisao(int a, int b){
+    private static int divisao(int a, int b) throws IllegalArgumentException{
        if (b == 0){
            throw new IllegalArgumentException("Argumento ilegal, não pode ser 0");
        }
