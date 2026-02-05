7833cab727ccd1c0599c91108cdccc9c39b53599
VitorD423 <vitordiego200@gmail.com>
2026-02-05 13:19:52 -0300

    Merge remote-tracking branch 'origin/master'

--- src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest03.java
+++ src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest03.java
@@ -2,12 +2,20 @@ package academy.devdojo.maratonajava.javacore.Oexception.runtime.test;
 
 public class RuntimeExceptionTest03 {
     public static void main(String[] args) {
+        abreConexao();
+    }
+
+    private static String abreConexao() {
         try {
             System.out.println("Abrindo arquivo");
             System.out.println("Escrevendo dados no arquivo");
-            System.out.println("Fechando o arquivo");
-        }catch (Exception e){
+            return "Conexão aberta";
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Fechando recurso liberado pelo SO");
 
         }
+        return null;
     }
 }
