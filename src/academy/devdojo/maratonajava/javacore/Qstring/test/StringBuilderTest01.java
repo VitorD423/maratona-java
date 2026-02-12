b275fbbba09fb973ed01d119febcb08fef9e1ee8
VitorD423 <vitordiego200@gmail.com>
2026-02-12 12:00:12 -0300

    Merge remote-tracking branch 'origin/master'

--- src/academy/devdojo/maratonajava/javacore/Qstring/test/StringBuilderTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Qstring/test/StringBuilderTest01.java
@@ -4,6 +4,13 @@ public class StringBuilderTest01 {
     public static void main(String[] args) {
         String nome = "William suane";
         nome.concat(" DevDojo");
+        nome = nome.substring(0,3);
         System.out.println(nome);
+        StringBuilder sb = new StringBuilder("William suane");
+        sb.append(" DevDojo").append(" Academy");
+        sb.reverse();
+        sb.reverse();
+        sb.delete(0,3);
+        System.out.println(sb);
     }
 }
