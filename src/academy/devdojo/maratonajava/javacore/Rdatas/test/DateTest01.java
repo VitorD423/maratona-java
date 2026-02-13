f27708628c9dbcb83cc024b751cf5b70f382e1bb
VitorD423 <vitordiego200@gmail.com>
2026-02-13 06:17:05 -0300

    Merge remote-tracking branch 'origin/master'

---
+++ src/academy/devdojo/maratonajava/javacore/Rdatas/test/DateTest01.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.javacore.Rdatas.test;
+
+import java.util.Date;
+
+public class DateTest01 {
+    public static void main(String[] args) {
+        Date date = new Date();
+    }
+}
