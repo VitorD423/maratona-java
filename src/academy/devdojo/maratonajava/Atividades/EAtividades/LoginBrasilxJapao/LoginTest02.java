d2c4cf6aeced403d5e5d60a75c2e313404988f50
VitorD423 <vitordiego200@gmail.com>
2026-03-04 08:42:14 -0300

    Merge remote-tracking branch 'origin/master'

--- src/academy/devdojo/maratonajava/Atividades/EAtividades/LoginBrasilxJapao/LoginTest02.java
+++ src/academy/devdojo/maratonajava/Atividades/EAtividades/LoginBrasilxJapao/LoginTest02.java
@@ -1,13 +1,21 @@
 package academy.devdojo.maratonajava.Atividades.EAtividades.LoginBrasilxJapao;
 
+import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class LoginTest02 {
     public static void main(String[] args) {
+        Instant loginInstante = Instant.now();
+
         String zonaUsuario = "Asia/Tokyo";
 
-        ZoneId zone = ZoneId.of(zonaUsuario);
+        ZonedDateTime dataUsuario = loginInstante.atZone(ZoneId.of(zonaUsuario));
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+
+        System.out.println("Horário do usuário: " + dataUsuario.format(formatter));
 
 
     }
