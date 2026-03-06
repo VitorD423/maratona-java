68736437bc4d8b00b3506ed95f8c4bbec58f2916
VitorD423 <vitordiego200@gmail.com>
2026-03-06 10:14:39 -0300

    Merge remote-tracking branch 'origin/master'

--- src/academy/devdojo/maratonajava/javacore/Uregex/test/PatternMatcherTest05.java
+++ src/academy/devdojo/maratonajava/javacore/Uregex/test/PatternMatcherTest05.java
@@ -1,9 +1,10 @@
 package academy.devdojo.maratonajava.javacore.Uregex.test;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class PatternMatcherTest04 {
+public class PatternMatcherTest05 {
     public static void main(String[] args) {
         // \d = Todos os digitos
         // \D = Tudo o que não for digito
@@ -22,13 +23,16 @@ public class PatternMatcherTest04 {
         // $
         // . 1.3 = 123, 133, 1@3, 1A3
 
-        String regex = "0[xX]([0-9a-fA-F])+(\\s|$)";
-        String texto = "12 0x 0X 0xFFABC 0x10G 0x1";
+        String regex = "([a-zA-Z0-9\\._-])+@([a-zA-Z])+(\\.([a-zA-Z])+)+";
+        String texto = "luffy@hotmail.com, 123jotaro@gmail.com, #@!zoro@mail.br, teste@gmail.com.br, sakura@mial";
+        System.out.println("Email valido");
+        System.out.println("#@!zoro@mail.br".matches(regex));
+        System.out.println(Arrays.toString(texto.split(",")));
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(texto);
-        System.out.println("texto:  "+texto);
-        System.out.println("indice: 0123456789");
-        System.out.println("regex " +regex);
+//        System.out.println("texto:  "+texto);
+//        System.out.println("indice: 0123456789");
+//        System.out.println("regex " +regex);
         System.out.println("Posicoes encontradas");
         while (matcher.find()){
             System.out.print(matcher.start()+ " "+ matcher.group()+"\n");
