8d2387477dbb032560dbfe730e23c0ada68f0768
VitorD423 <vitordiego200@gmail.com>
2026-03-11 20:16:42 -0300

    Merge remote-tracking branch 'origin/master'

--- src/academy/devdojo/maratonajava/Atividades/EAtividades/File/Test/FileTest01.java
+++ src/academy/devdojo/maratonajava/Atividades/EAtividades/File/Test/FileTest01.java
@@ -1,7 +1,28 @@
 package academy.devdojo.maratonajava.Atividades.EAtividades.File.Test;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileTest01 {
     public static void main(String[] args) {
+        File file = new File("nomes.txt");
+        try {
+            boolean isCreate = file.createNewFile();
 
+            try (FileWriter fw = new FileWriter(file)){
+                fw.write("Naruto Goku Luffy");
+            }
+
+            try (FileReader fr = new FileReader(file)){
+                int i;
+                while ((i = fr.read()) != -1){
+                    System.out.print((char) i);
+                }
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
