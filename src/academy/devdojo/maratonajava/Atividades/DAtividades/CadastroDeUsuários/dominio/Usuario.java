baa897dd8ce98e2fea43b3aaaf2bec384a7e112c
VitorD423 <vitordiego200@gmail.com>
2026-03-15 12:26:00 -0300

    Merge remote-tracking branch 'origin/master'

--- src/academy/devdojo/maratonajava/Atividades/DAtividades/CadastroDeUsuários/dominio/Usuario.java
+++ src/academy/devdojo/maratonajava/Atividades/DAtividades/CadastroDeUsuários/dominio/Usuario.java
@@ -6,7 +6,7 @@ public class Usuario {
 
     public Usuario(String nome, String email, int idade) {
 
-        if (nome.isBlank()){
+         if (nome.isBlank()){
             throw new NomeInvalidoException("Nome invalido");
         }
 
