fc93bc7539a1e298a67c92dc6ede8c0cdd89c2f0
VitorD423 <vitordiego200@gmail.com>
2026-03-17 00:07:34 -0300

    Merge remote-tracking branch 'origin/master'

--- src/academy/devdojo/maratonajava/javacore/Wnio/test/PathTest03.java
+++ src/academy/devdojo/maratonajava/javacore/Wnio/test/PathTest03.java
@@ -9,5 +9,16 @@ public class PathTest03 {
         Path arquivo = Paths.get("dev/arquivo.txt");
         Path resolve = dir.resolve(arquivo);
         System.out.println(resolve);
+
+        Path absoluto = Paths.get("/home/vitor");
+        Path relativo = Paths.get("dev");
+        Path file = Paths.get("file.txt");
+
+        System.out.println("1 "+absoluto.resolve(relativo));
+        System.out.println("2 "+absoluto.resolve(file));
+        System.out.println("3 "+absoluto.resolve(absoluto));
+        System.out.println("4 "+absoluto.resolve(file));
+        System.out.println("5 "+absoluto.resolve(absoluto));
+        System.out.println("6 "+absoluto.resolve(relativo));
     }
 }
