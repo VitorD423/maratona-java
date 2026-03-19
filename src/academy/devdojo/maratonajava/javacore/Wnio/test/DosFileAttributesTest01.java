58aed2ae166350fef58dfd607820487476d23290
VitorD423 <vitordiego200@gmail.com>
2026-03-19 10:03:08 -0300

    oi9k

---
+++ src/academy/devdojo/maratonajava/javacore/Wnio/test/DosFileAttributesTest01.java
@@ -0,0 +1,30 @@
+package academy.devdojo.maratonajava.javacore.Wnio.test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.DosFileAttributeView;
+import java.nio.file.attribute.DosFileAttributes;
+
+public class DosFileAttributesTest01 {
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("pasta/teste.txt");
+        if (Files.notExists(path)) Files.createFile(path);
+        DosFileAttributes dosFileAttributes = Files.readAttributes(path, DosFileAttributes.class);
+        System.out.println(dosFileAttributes.isHidden());
+        System.out.println(dosFileAttributes.isReadOnly());
+
+        DosFileAttributeView fileAttributeView = Files.getFileAttributeView(path, DosFileAttributeView.class);
+        fileAttributeView.setHidden(true);
+        fileAttributeView.setReadOnly(true);
+
+
+        //VAI TRANSFORMAR O ARQUIVO EM OCULTO E DEIXAR APENAS PARA LEITURA
+//        Files.setAttribute(path,"dos:hidden", true);
+//        Files.setAttribute(path,"dos:readonly", true);
+//        //Pra voltar ao normal basta trocar para false
+//        Files.setAttribute(path,"dos:hidden", false);
+//        Files.setAttribute(path,"dos:readonly", false);
+    }
+}
