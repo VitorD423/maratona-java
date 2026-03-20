9fdb9898454cbf9b2987b1569b41a138493dbd3f
VitorD423 <vitordiego200@gmail.com>
2026-03-20 15:42:32 -0300

    oi9k

--- src/academy/devdojo/maratonajava/javacore/Wnio/test/SimpleFileVisitorTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Wnio/test/SimpleFileVisitorTest02.java
@@ -7,32 +7,39 @@ import java.nio.file.attribute.BasicFileAttributes;
 class ListAllFiles extends SimpleFileVisitor<Path>{
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs){
-        if (file.getFileName().toString().endsWith(".java")){
         System.out.println(file.getFileName());
-       }
         return FileVisitResult.CONTINUE;
     }
 
     @Override
     public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-        return super.preVisitDirectory(dir, attrs);
+        System.out.println("pre visit "+dir.getFileName());
+        return FileVisitResult.CONTINUE;
+        //QUANDO ENTRA NA PASTA
+        //SE USAR O SKIP_SUBTREE NO LUGAR DO CONTINUE
+        //ELE VAI PULAR TUDO QUE ESTA DENTRO DA PASTA "pasta"
+        // O SKIP_SIBLINGS VAI IGNORAR TODOS OS "IRMÃOS"
     }
 
     @Override
     public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
         return super.visitFileFailed(file, exc);
+        //QUANDO TENTAR LER O ARQUIVO E ELE NÃO TIVER PERMISSÃO DE LEITURA
+        //QUALQUER TIPO DE PROBLEMA PARA PEGAR O ARQUIVO
     }
 
     @Override
     public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
-        return super.postVisitDirectory(dir, exc);
+        System.out.println("post visit "+dir.getFileName());
+        return FileVisitResult.CONTINUE;
+        //QUANDO ESTÁ SAINDO DA PASTA
     }
 }
 
 public class SimpleFileVisitorTest02 {
     public static void main(String[] args) throws IOException {
         //COMO FUNCIONA
-        Path root = Paths.get(".");
-        Files.walkFileTree(root, new ListJavaFiles()); //O segundo parametro é um file visitor
+        Path root = Paths.get("pasta");
+        Files.walkFileTree(root, new ListAllFiles()); //O segundo parametro é um file visitor
     }
 }
