4d406ee3789a21655c1d33d4719d824ef1787c6c
VitorD423 <vitordiego200@gmail.com>
2026-03-20 10:30:06 -0300

    oi9k

--- src/academy/devdojo/maratonajava/javacore/Wnio/test/SimpleFileVisitorTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Wnio/test/SimpleFileVisitorTest02.java
@@ -4,24 +4,32 @@ import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 
-class ListJavaFiles extends SimpleFileVisitor<Path>{
+class ListAllFiles extends SimpleFileVisitor<Path>{
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs){
-       //ESSE if VAI MOSTRAR APENAS OS .JAVA
-
         if (file.getFileName().toString().endsWith(".java")){
         System.out.println(file.getFileName());
        }
         return FileVisitResult.CONTINUE;
+    }
 
-                //CONTINUE
-                //TEMINATE
-                //SKIP_SUBTREE
-                //SKIP_SIBLINGS
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        return super.preVisitDirectory(dir, attrs);
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        return super.visitFileFailed(file, exc);
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        return super.postVisitDirectory(dir, exc);
     }
 }
 
-public class SimpleFileVisitorTest01 {
+public class SimpleFileVisitorTest02 {
     public static void main(String[] args) throws IOException {
         //COMO FUNCIONA
         Path root = Paths.get(".");
