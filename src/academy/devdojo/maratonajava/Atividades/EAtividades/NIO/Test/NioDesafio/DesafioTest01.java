fcd49312422b70aa7039020a92f3ebbc01da2aa8
VitorD423 <vitordiego200@gmail.com>
2026-03-21 11:35:18 -0300

    oi9k

---
+++ src/academy/devdojo/maratonajava/Atividades/EAtividades/NIO/Test/NioDesafio/DesafioTest01.java
@@ -0,0 +1,41 @@
+package academy.devdojo.maratonajava.Atividades.EAtividades.NIO.Test.NioDesafio;
+
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+
+class ListAllFiles extends SimpleFileVisitor<Path>{
+    private List<String> relatorio;
+    private PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:**/*.{java}");
+
+    public ListAllFiles(List<String> relatorio){
+        this.relatorio = relatorio;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (matcher.matches(file) && attrs.size() < 1024) {
+
+            String linha = file.getFileName()
+                    + " - " + attrs.size() + " bytes";
+
+            relatorio.add(linha);
+        }
+
+        return FileVisitResult.CONTINUE;
+    }
+}
+
+
+public class DesafioTest01 {
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get(".");
+        List<String> relatorio = new ArrayList<>();
+        Files.walkFileTree(path, new ListAllFiles(relatorio));
+        Path output = Paths.get("relatorio.txt");
+        Files.write(output,relatorio);
+    }
+}
