f0ba6cf2c8771325e8cb1c89b2cf561c97ca584c
VitorD423 <vitordiego200@gmail.com>
2026-03-22 08:20:07 -0300

    oi9k

--- src/academy/devdojo/maratonajava/Atividades/EAtividades/NIO/Test/NioDesafio/DesafioTest01.java
+++ src/academy/devdojo/maratonajava/Atividades/EAtividades/NIO/Test/NioDesafio/DesafioTest01.java
@@ -4,7 +4,10 @@ package academy.devdojo.maratonajava.Atividades.EAtividades.NIO.Test.NioDesafio;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 class ListAllFiles extends SimpleFileVisitor<Path>{
@@ -17,10 +20,22 @@ class ListAllFiles extends SimpleFileVisitor<Path>{
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+
+        var creationTime = attrs.creationTime();
+        var lastModifiedTime = attrs.lastModifiedTime();
+
+        Date dateCriacao = new Date(creationTime.toMillis());
+        Date dateModificacao = new Date(lastModifiedTime.toMillis());
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+        String dataCriacaoFormatada = simpleDateFormat.format(dateCriacao);
+        String dataModFormatada = simpleDateFormat.format(dateModificacao);
+
+
         if (matcher.matches(file) && attrs.size() > 1024) {
 
-            String linha = file.getFileName()
-                    + " - " + attrs.size() + " bytes";
+            String linha = file.getFileName() + " - " + attrs.size() + " bytes" + " - Criado: " + dataCriacaoFormatada + " - Modificado: " + dataModFormatada;
 
             relatorio.add(linha);
         }
