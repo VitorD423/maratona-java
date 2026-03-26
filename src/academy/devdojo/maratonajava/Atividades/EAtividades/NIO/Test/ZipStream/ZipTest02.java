118795a5355fe00260151a6cfc3d6d10ce2d72da
VitorD423 <vitordiego200@gmail.com>
2026-03-26 08:09:10 -0300

    oi9k

---
+++ src/academy/devdojo/maratonajava/Atividades/EAtividades/NIO/Test/ZipStream/ZipTest02.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.Atividades.EAtividades.NIO.Test.ZipStream;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.zip.ZipOutputStream;
+
+public class ZipTest02 {
+    public static void main(String[] args) throws IOException {
+        Path path1 = Paths.get("Pagina1.txt");
+        Path path2 = Paths.get("Pagina2.txt");
+        Files.writeString(path1, "ESCRITA PAGINA 1");
+        Files.writeString(path2, "ESCRITA PAGINA 2");
+        Path zip = Paths.get("arquivo.zip");
+
+        try (ZipOutputStream zop = new ZipOutputStream(Files.newOutputStream(zip))){
+
+        }
+    }
+}
