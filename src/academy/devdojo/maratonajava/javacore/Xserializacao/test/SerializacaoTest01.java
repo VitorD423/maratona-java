d4a93e3e61db7fe90b8b1b594d114925eacd79b5
VitorD423 <vitordiego200@gmail.com>
2026-03-27 07:51:17 -0300

    oi9k

---
+++ src/academy/devdojo/maratonajava/javacore/Xserializacao/test/SerializacaoTest01.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Xserializacao.test;
+
+import academy.devdojo.maratonajava.javacore.Xserializacao.dominio.Aluno;
+
+public class SerializacaoTest01 {
+    public static void main(String[] args) {
+        Aluno aluno1 = new Aluno(1L,"Vitor Diego","12345678");
+    }
+        //Como salvar objeto em um arquivo e depois ler
+    private static void serializar(){
+
+    }
+}
