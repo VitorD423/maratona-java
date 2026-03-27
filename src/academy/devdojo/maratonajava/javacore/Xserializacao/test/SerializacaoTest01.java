654efc9c1eec46968ac0f654d3a123dd035945e7
VitorD423 <vitordiego200@gmail.com>
2026-03-27 07:58:32 -0300

    oi9k

--- src/academy/devdojo/maratonajava/javacore/Xserializacao/test/SerializacaoTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Xserializacao/test/SerializacaoTest01.java
@@ -2,12 +2,24 @@ package academy.devdojo.maratonajava.javacore.Xserializacao.test;
 
 import academy.devdojo.maratonajava.javacore.Xserializacao.dominio.Aluno;
 
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class SerializacaoTest01 {
     public static void main(String[] args) {
-        Aluno aluno1 = new Aluno(1L,"Vitor Diego","12345678");
+        Aluno aluno = new Aluno(1L,"Vitor Diego","12345678");
+        serializar(aluno);
     }
         //Como salvar objeto em um arquivo e depois ler
-    private static void serializar(){
-
+    private static void serializar(Aluno aluno){
+        Path path = Paths.get("pasta/aluno.ser");
+        try (ObjectOutputStream oos = new ObjectOutputStream(Files.newOutputStream(path))){
+            oos.writeObject(aluno);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
