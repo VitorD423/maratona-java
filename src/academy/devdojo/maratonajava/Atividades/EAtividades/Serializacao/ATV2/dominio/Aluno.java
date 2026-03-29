b698802c380415d2f2b21e4f4e91b4c9baf780c1
VitorD423 <vitordiego200@gmail.com>
2026-03-29 09:02:19 -0300

    oi9k

--- src/academy/devdojo/maratonajava/Atividades/EAtividades/Serializacao/ATV2/dominio/Aluno.java
+++ src/academy/devdojo/maratonajava/Atividades/EAtividades/Serializacao/ATV2/dominio/Aluno.java
@@ -1,6 +1,8 @@
 package academy.devdojo.maratonajava.Atividades.EAtividades.Serializacao.ATV2.dominio;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Aluno implements Serializable {
     @Serial
@@ -8,6 +10,7 @@ public class Aluno implements Serializable {
     private String nome;
     private  int id;
     private transient String senha;
+    private transient List<Materias> materias;
 
 
     public Aluno(String nome, int id, String senha) {
@@ -18,9 +21,18 @@ public class Aluno implements Serializable {
 
     @Serial
 private void writeObject(ObjectOutputStream oos){
-    try {
+        int size = (materias == null) ? 0 : materias.size();
+        try {
             oos.defaultWriteObject();
+            oos.writeInt(size);
             oos.writeUTF(senha);
+            if (materias != null ){
+                for (Materias materia : materias){
+                    oos.writeUTF(materia.getNome());
+                    oos.writeDouble(materia.getValor());
+                }
+            }
+
         }catch (IOException e){
         e.printStackTrace();
     }
@@ -29,7 +41,15 @@ private void writeObject(ObjectOutputStream oos){
 private void readObject(ObjectInputStream ois){
         try {
             ois.defaultReadObject();
+            int tamanho = ois.readInt();
             senha = ois.readUTF();
+            materias = new ArrayList<>();
+            for (int i = 0 ; i < tamanho; i ++){
+                String nome = ois.readUTF();
+                double preco = ois.readDouble();
+                Materias materias1 = new Materias(nome,preco);
+                materias.add(materias1);
+            }
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
@@ -41,6 +61,7 @@ private void readObject(ObjectInputStream ois){
                 "nome='" + nome + '\'' +
                 ", id=" + id +
                 ", senha='***" + '\'' +
+                ", Matérias =" + materias +
                 '}';
     }
 
