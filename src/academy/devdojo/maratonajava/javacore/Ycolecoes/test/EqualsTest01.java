7f36532e5db314d52d12a42221378559f133b4d8
VitorD423 <vitordiego200@gmail.com>
2026-03-30 07:21:24 -0300

    oi9k

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/EqualsTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/EqualsTest01.java
@@ -5,25 +5,7 @@ import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Smartphone;
 public class EqualsTest01 {
     public static void main(String[] args) {
         Smartphone s1 = new Smartphone("1ABC1","iPhone");
-        Smartphone s2 = s1;
+        Smartphone s2 = new Smartphone("1ABC1","Pixel");
         System.out.print(s1.equals(s2));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        //        String nome = "William Suana";
-//        String nome2 =new String ("William Suana");
-//        System.out.println(nome.equals(nome2));
-
     }
 }
