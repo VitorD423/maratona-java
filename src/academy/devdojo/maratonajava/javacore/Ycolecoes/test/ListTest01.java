e3a242f2b507296d354fb75da94a51d1f447fbf2
VitorD423 <vitordiego200@gmail.com>
2026-03-30 08:21:44 -0300

    .

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListTest01.java
@@ -5,18 +5,28 @@ import java.util.List;
 
 public class ListTest01 {
     public static void main(String[] args) {
-        List<String> nomes = new ArrayList<>(); //1.4 <> força o tipo
+        List<String> nomes = new ArrayList<>(16); //1.4 <> força o tipo
+        List<String> nomes2 = new ArrayList<>(16); //1.4 <> força o tipo
         nomes.add("William");
-        nomes.add("DevDojo Academy");
+        nomes.add("DevDojo");
+        nomes2.add("Suane");
+        nomes2.add("Academy");
 
+        //System.out.println(nomes.remove("william"));
+        nomes.addAll(nomes2);
 
         for (String nome: nomes) {
             System.out.println(nome);
         }
-        nomes.add("Vitor");
+
         System.out.println("----------------");
+
+        int size = nomes.size();
         for (int i = 0; i < nomes.size(); i++) {
             System.out.println(nomes.get(i));
         }
+
+        List<Integer> numeros = new ArrayList<>();
+        numeros.add(1);
     }
 }
