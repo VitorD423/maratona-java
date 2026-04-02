860b6611d13d3aa7f7300fcb89eed14289c4b4f0
VitorD423 <vitordiego200@gmail.com>
2026-04-02 09:43:19 -0300

    .

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListSortTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListSortTest01.java
@@ -12,9 +12,23 @@ public class ListSortTest01 {
         mangas.add("Pokemon");
         mangas.add("Attack on titan");
         mangas.add("Dragon ball Z");
+
         Collections.sort(mangas);
+
+        List<Double> dinheiros = new ArrayList<>();
+
+        dinheiros.add(100.21);
+        dinheiros.add(23.98);
+        dinheiros.add(21.21);
+        dinheiros.add(98.10);
+
+        System.out.println(dinheiros);
+
+        Collections.sort(dinheiros);
+
         for (String manga: mangas){
             System.out.println(manga);
         }
+        System.out.println(dinheiros);
     }
 }
