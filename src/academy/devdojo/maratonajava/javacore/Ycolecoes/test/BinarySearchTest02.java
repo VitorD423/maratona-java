5b000e6b85d7f4aa46d87d4d193627612778e2a8
VitorD423 <vitordiego200@gmail.com>
2026-04-07 18:05:41 -0300

    .

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/BinarySearchTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/BinarySearchTest02.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class BinarySearchTest02 {
     public static void main(String[] args) {
         MangaByIdComparator mangaByIdComparator = new MangaByIdComparator();
-                List < Manga > mangas = new ArrayList<>(6);
+                List < Manga > mangas = new ArrayList<>(7);
         mangas.add(new Manga(5L,"Hellsing Ultimate",19.9));
         mangas.add(new Manga(1L,"Berserk",9.5));
         mangas.add(new Manga(4L,"Pokemon",3.2));
