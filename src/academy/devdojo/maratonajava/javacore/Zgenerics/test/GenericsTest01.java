05e9c5a894aa1fc07fc372975f22d4b72a4fdff4
VitorD423 <vitordiego200@gmail.com>
2026-04-20 09:56:28 -0300

    .

--- src/academy/devdojo/maratonajava/javacore/Zgenerics/test/GenericsTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Zgenerics/test/GenericsTest01.java
@@ -1,7 +1,22 @@
 package academy.devdojo.maratonajava.javacore.Zgenerics.test;
 
+import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Consumidor;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class GenericsTest01 {
     public static void main(String[] args) {
+        //Conceito type erasure apaga o tipo depois do codigo ser compilado
+        List<String> lista = new ArrayList<>();
+        lista.add("Midoriya");
+        lista.add("Midoriya");
+        lista.add("Midoriya");
+
+        for (Object o : lista) {
+            System.out.println(o);
+        }
 
     }
+
 }
