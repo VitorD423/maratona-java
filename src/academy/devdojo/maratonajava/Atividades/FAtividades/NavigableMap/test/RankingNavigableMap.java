db766da3f25a76f0cc8160da7b2601f52d86589e
VitorD423 <vitordiego200@gmail.com>
2026-04-26 08:55:06 -0300

    .

--- src/academy/devdojo/maratonajava/Atividades/FAtividades/NavigableMap/test/RankingNavigableMap.java
+++ src/academy/devdojo/maratonajava/Atividades/FAtividades/NavigableMap/test/RankingNavigableMap.java
@@ -1,39 +1,44 @@
 package academy.devdojo.maratonajava.Atividades.FAtividades.NavigableMap.test;
 
-import java.util.Map;
-import java.util.NavigableMap;
-import java.util.TreeMap;
+import java.util.*;
 
 public class RankingNavigableMap {
     public static void main(String[] args) {
-        NavigableMap<Integer, String> ranking = new TreeMap<>();
-        ranking.put(100,"Pedro");
-        ranking.put(300,"Ana");
-        ranking.put(200,"Carlos");
-        ranking.put(200,"Pedro");
+        NavigableMap<Integer, List<String>> ranking = new TreeMap<>();
+        ranking.computeIfAbsent(100,k -> new ArrayList<>()).add("Carlos");
+        ranking.computeIfAbsent(300,k -> new ArrayList<>()).add("Ana");
+        ranking.computeIfAbsent(200,k -> new ArrayList<>()).add("Carlos");
+        ranking.computeIfAbsent(200,k -> new ArrayList<>()).add("Pedro");
 
         System.out.println("Maior pro menor: ");
-        for (Map.Entry<Integer,String> entry: ranking.descendingMap().entrySet()){
+        for (Map.Entry<Integer,List<String>> entry: ranking.descendingMap().entrySet()){
             System.out.println(entry.getKey() + " - " + entry.getValue());
         }
 
         System.out.println("-------------------------");
 
         System.out.println("Abaixo de 200: ");
-            Map.Entry<Integer, String> lower = ranking.lowerEntry(200);
+            Map.Entry<Integer, List<String>> lower = ranking.lowerEntry(200);
             System.out.println(lower.getKey() + " - " + lower.getValue());
 
         System.out.println("-------------------------");
 
         System.out.println("Acima de 200: ");
-        Map.Entry<Integer, String> high = ranking.higherEntry(200);
+        Map.Entry<Integer, List<String>> high = ranking.higherEntry(200);
         System.out.println(high.getKey() + " - " + high.getValue());
 
         System.out.println("-------------------------");
 
         System.out.println("TOP 1 : ");
-        Map.Entry<Integer, String> top = ranking.lastEntry();
+        Map.Entry<Integer, List<String>> top = ranking.lastEntry();
         System.out.println(top.getKey() + " - " + top.getValue());
+
+        System.out.println("-------------------------");
+
+        System.out.println("Remover o pior jogador: ");
+        Map.Entry<Integer, List<String>> pior = ranking.pollFirstEntry();
+        System.out.println(pior.getKey() + " - " + pior.getValue());
+
 
     }
 }
