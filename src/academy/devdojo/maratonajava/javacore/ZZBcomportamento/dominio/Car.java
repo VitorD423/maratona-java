c445058132bbb264135b49d694fc569819685166
VitorD423 <vitordiego200@gmail.com>
2026-05-08 10:05:55 -0300

    .

---
+++ src/academy/devdojo/maratonajava/javacore/ZZBcomportamento/dominio/Car.java
@@ -0,0 +1,24 @@
+package academy.devdojo.maratonajava.javacore.ZZBcomportamento.dominio;
+
+public class Car {
+    private String name = "Audi";
+    private String color;
+    private int year;
+
+    public Car(String color, int year) {
+        this.color = color;
+        this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/ZZBcomportamento/test/ComportamentoPorParametroTest01.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.javacore.ZZBcomportamento.test;
+
+import academy.devdojo.maratonajava.javacore.ZZBcomportamento.dominio.Car;
+
+import java.util.List;
+
+public class ComportamentoPorParametroTest01 {
+
+    public static void main(String[] args) {
+        List<Car> cars = List.of(new Car("green", 2011),
+                                new Car("black", 1998),
+                                new Car("red",2019));
+
+    }
+}
