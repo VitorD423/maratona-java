ce3f430924b387d2ba6845ca8f9ea30444d3f970
VitorD423 <vitordiego200@gmail.com>
2026-05-26 18:51:16 -0300

    .

---
+++ src/academy/devdojo/maratonajava/Atividades/FAtividades/Streams/flatMap/flatMapTest01.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.Atividades.FAtividades.Streams.flatMap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class flatMapTest01 {
+    public static void main(String[] args) {
+        List<String> guerreiros = List.of("Thorin","Guts","Arthur");
+        List<String> magos = List.of("Merlin","Ainz","Frieren");
+        List<String> arqueiros = List.of("Legolas", "Hanzo","Sylvanas");
+
+        List<List<String>> guildas = new ArrayList<>();
+        guildas.add(guerreiros);
+        guildas.add(magos);
+        guildas.add(arqueiros);
+
+        for (List<String> guilda : guildas) {
+            for (String s : guilda) {
+                System.out.println(s);
+            }
+
+        }
+
+    }
+}
