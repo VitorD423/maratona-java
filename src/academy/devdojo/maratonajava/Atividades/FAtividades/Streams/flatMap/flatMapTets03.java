3802b9b658d9a6a4afcdf7b2ab0e6cf23daa4576
VitorD423 <vitordiego200@gmail.com>
2026-05-29 21:29:55 -0300

    .

--- src/academy/devdojo/maratonajava/Atividades/FAtividades/Streams/flatMap/flatMapTets03.java
+++ src/academy/devdojo/maratonajava/Atividades/FAtividades/Streams/flatMap/flatMapTets03.java
@@ -14,5 +14,7 @@ public class flatMapTets03 {
         inventarios.add(bolsa1);
         inventarios.add(bolsa2);
         inventarios.add(bolsa3);
+
+        inventarios.stream().flatMap(List::stream).forEach(System.out::println);
     }
 }
