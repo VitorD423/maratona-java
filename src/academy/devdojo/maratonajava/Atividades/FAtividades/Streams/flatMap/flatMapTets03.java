386bac0362658e4df757cf2416e25d3a394d94a9
VitorD423 <vitordiego200@gmail.com>
2026-05-29 22:17:11 -0300

    .

--- src/academy/devdojo/maratonajava/Atividades/FAtividades/Streams/flatMap/flatMapTets03.java
+++ src/academy/devdojo/maratonajava/Atividades/FAtividades/Streams/flatMap/flatMapTets03.java
@@ -1,7 +1,9 @@
 package academy.devdojo.maratonajava.Atividades.FAtividades.Streams.flatMap;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class flatMapTets03 {
     public static void main(String[] args) {
@@ -15,6 +17,15 @@ public class flatMapTets03 {
         inventarios.add(bolsa2);
         inventarios.add(bolsa3);
 
+
+
+
         inventarios.stream().flatMap(List::stream).forEach(System.out::println);
+        System.out.println();
+        List<String> l1 = bolsa1.stream().map(w -> w.split("")).flatMap(Arrays::stream).distinct().collect(Collectors.toList());
+        List<String> l2 = bolsa2.stream().map(w -> w.split("")).flatMap(Arrays::stream).distinct().collect(Collectors.toList());
+        List<String> l3 = bolsa3.stream().map(w -> w.split("")).flatMap(Arrays::stream).distinct().collect(Collectors.toList());
+
+        System.out.println("Bolsa 1: " + l1 +" Bolsa 2: "+ l2 +" Bolsa 3: "+ l3);
     }
 }
