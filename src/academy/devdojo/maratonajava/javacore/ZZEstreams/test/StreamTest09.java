c7601b33de3a97647e6a226d7248d224a6f9e5e3
VitorD423 <vitordiego200@gmail.com>
2026-06-06 16:53:50 -0300

    .

--- src/academy/devdojo/maratonajava/javacore/ZZEstreams/test/StreamTest09.java
+++ src/academy/devdojo/maratonajava/javacore/ZZEstreams/test/StreamTest09.java
@@ -1,9 +1,17 @@
 package academy.devdojo.maratonajava.javacore.ZZEstreams.test;
 
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class StreamTest09 {
     public static void main(String[] args) {
         IntStream.rangeClosed(1,50).filter(n -> n % 2 == 0).forEach(n -> System.out.println(n + " "));
+        System.out.println();
+        IntStream.range(1,50).filter(n -> n % 2 == 0).forEach(n -> System.out.println(n + " "));
+
+
+        Stream.of("Eleve "," O"," Cosmo"," no seu coração")
+                .map(String::toUpperCase)
+                .forEach(s -> System.out.println(s +" "));
     }
 }
